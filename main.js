5505a1e3e656d7871fc6f3a47989866102cba95f
Labib Hasan <labibshawon101@gmail.com>
2025-05-21 23:18:02 +0600

    update

---
+++ main.js
@@ -0,0 +1,10 @@
+var biscuitJar = "Lexus";
+console.log(biscuitJar)
+
+const potatoPacket = "Lays";
+console.log(potatoPacket)
+
+biscuitJar = ["lexus", "orio", "orange", "true", "false",
+    "null", ( 1, 2, 3)
+];
+console.log(biscuitJar )
