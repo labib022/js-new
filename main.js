9f2c089dac5210d6e2653425ad0bcb4d8b79c78d
Labib Hasan <labibshawon101@gmail.com>
2025-05-24 12:16:51 +0600

    new-update

--- main.js
+++ main.js
@@ -1,10 +1,44 @@
-var biscuitJar = "Lexus";
-console.log(biscuitJar)
+// var biscuitJar = "Lexus";
+// console.log(biscuitJar);
 
-const potatoPacket = "Lays";
-console.log(potatoPacket)
+// const myName = document.querySelector('.panda');
+// myName.innerHTML = "shawon";
+// console.log(myName)
 
-biscuitJar = ["lexus", "orio", "orange", "true", "false",
-    "null", ( 1, 2, 3)
-];
-console.log(biscuitJar )
+// const potatoPacket = "Lays";
+// console.log(potatoPacket);
+
+// const a = [1,2,3,4];
+// console.log(a[0]);
+
+// biscuitJar = ["lexus", "orio", "orange", "true", "false",
+//     "null", ( 1, 2, 3)
+// ];
+// console.log(biscuitJar )
+
+// person = {
+//     name: "Leo Messi" ,
+//     country: " Argentina",
+//     profession: "Footballer ",
+//     greencard:  true,
+//     age: 37,
+
+//     surName: ["Leo", "Andress", "messi"],
+//     father: {
+//         name: "Horhe Messi",
+//         age: 58,
+//     }
+
+// } 
+
+// console.log(person['profession'][0]);
+
+function square(number){
+    return number * number
+}
+    
+
+const result = square(10)
+console.log(result);
+
+
